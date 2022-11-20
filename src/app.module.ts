5f83e1d6f67a9f66cb04a882b735b63a5a183143
Maykon Lacerda <maykon@smartinnovation.com.br>
2022-11-20 02:01:53 -0300

    fixup! chore: improve authentication flow

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,10 @@ import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
 
 @Module({
+  imports: [
     ConfigModule.forRoot({ isGlobal: true, load: [config] }),
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
